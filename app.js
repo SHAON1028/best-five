450e0bd88a24a841154ed38ab0f75434c841f479
shaone <shaon8787@gmail.com>
2022-08-23 14:47:39 +0600

    Function addToFive Correctiond

--- app.js
+++ app.js
@@ -13,26 +13,68 @@ function display(arr) {
     }
 }
 
+function calulateTotal() {
+    const l = array.length
+    const valueOfPerPlayer = getValue('player-cost')
+    const totalPlayerVslue = l * valueOfPerPlayer
+    return totalPlayerVslue
+}
+//input value
+function getValue(elementId) {
+    const costField = document.getElementById(elementId)
+    const costString = costField.value
+    const cost = parseInt(costString)
+    return cost
+}
+
+function setTotalExpenses(elementId, expense) {
+
+    const totalCost = document.getElementById(elementId)
+    totalCost.innerText = expense
+
+}
+
 function addToTopfive(element, id) {
 
 
-    const productName = element.parentNode.children[0].innerText
-    const dis = document.getElementById(id)
-    dis.style.background = 'gray'
-    dis.innerText = 'selected'
-    dis.setAttribute('disabled', '');
-    array.push(productName)
+
+    // array.push(productName)
     const l = array.length
 
 
-    if (l <= 5) {
-        document.getElementById('total-player').innerText = l
+    if (l < 5) {
+        const productName = element.parentNode.children[0].innerText
+        const dis = document.getElementById(id)
+        dis.style.background = 'gray'
+        dis.innerText = 'selected'
+        dis.setAttribute('disabled', '');
+        array.push(productName)
+        document.getElementById('total-player').innerText = l + 1
         display(array)
+
+
     } else {
         alert('Maximum 5 selected')
     }
 }
 
+document.getElementById('calulate-btn').addEventListener('click', function () {
+    const playersvalue = calulateTotal()
+    setTotalExpenses('player-expenses', playersvalue)
+
+})
 
 
+document.getElementById('final-total-btn').addEventListener('click', function () {
+    const getTotalPlayerCost = document.getElementById('player-expenses')
+    const totalPlayerCostString = getTotalPlayerCost.innerText
+    const totalPlayerCost = parseInt(totalPlayerCostString)
 
+    const managerCost = getValue('manager-field')
+    const coachCost = getValue('coach-field')
+
+    const finalTotalCost = totalPlayerCost + managerCost + coachCost
+
+    setTotalExpenses('final-total', finalTotalCost)
+
+})
